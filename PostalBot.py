e30d70ab3dc2988cff751fe15768e7fed054754b
Lauri Junkkari <latenssi@gmail.com>
2016-03-27 11:23:38 +0300

    Minor tweaks.

--- PostalBot.py
+++ PostalBot.py
@@ -6,15 +6,11 @@ from requests import request
 from werkzeug.contrib.cache import SimpleCache
 
 
-STR_USERNAME = 'USERNAME'
-STR_MESSAGE_SENT = "Message sent"
-
-class PostalBot(object):
+class TelegramBot(object):
 
     def __init__(self, tg_api_key, tg_lounge_id):
         self.tg_api_key = tg_api_key
         self.tg_lounge_id = tg_lounge_id
-        self.cache = SimpleCache()
 
     def send_tg_message(self, chat_id, text):
         request('get', 'https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={text}'.format(
@@ -23,18 +19,27 @@ class PostalBot(object):
             text=text
         ))
 
+class PostalBot(TelegramBot):
+    STR_USERNAME = 'USERNAME'
+    STR_MESSAGE_SENT = "Message sent"
+    SPAM_TIMEOUT = 10*60
+
+    def __init__(self, tg_api_key, tg_lounge_id):
+        super(PostalBot, self).__init__(tg_api_key, tg_lounge_id)ssad
+        self.cache = SimpleCache()
+
     def handle_stream_publish(self, data):
         keys = data.keys()
         if 'watch_url' in keys and 'username' in keys:
-            c_key = STR_USERNAME + ':' + data['username']
+            c_key = self.STR_USERNAME + ':' + data['username']
             if not self.cache.get(c_key):
                 message = '{username} went live on Postal\n{url}'.format(
                     username=data['username'],
                     url=data['watch_url']
                 )
                 self.send_tg_message(self.tg_lounge_id, message)
-                self.cache.set(c_key, True, timeout=10*60)
-                return STR_MESSAGE_SENT
+                self.cache.set(c_key, True, timeout=self.SPAM_TIMEOUT)
+                return self.STR_MESSAGE_SENT
         return ''
 
     def handle_postal_new_post(self, data):
@@ -58,7 +63,7 @@ class PostalBot(object):
                 )
 
             self.send_tg_message(self.tg_lounge_id, message)
-            return STR_MESSAGE_SENT
+            return self.STR_MESSAGE_SENT
         return ''
 
     def handle_tg_update(self, data):
